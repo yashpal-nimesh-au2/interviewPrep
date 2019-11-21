18744587f4d6e521f5d2e8d942465aa1961daa15
yashpal-nimesh-au2 <yash.nimesh456@gmail.com>
2019-11-21 14:54:45 +0530

    week 2 day 3 challenge

--- Week2/Day3/challenge/index.js
+++ Week2/Day3/challenge/index.js
@@ -1,11 +1,41 @@
 
+var rotateRight = function(head, k) {
+    let array_nodes = [];
+    let last = head;
+    
+    array_nodes.push(last);
+    
+    if (last === null || last.next === null) {
+        return head;
+    }
+    
+    while (last.next !== null) {
+        last = last.next;
+        array_nodes.push(last);        
+    }
+    if (array_nodes.length > k+1) {
+        let size = array_nodes.length;
+        array_nodes.splice(0, size - 1 - k);
+    }
+    
+    k = (k % (array_nodes.length));
+    
+    let last_id = array_nodes.length - 1;
+    
+    for (let i = 0; i < k; i++) {
+        head = rotateListWithArray(head, array_nodes, last_id);
+        last_id--;
+    }
 
-function Product(a,b){
-let num1=a,num2=b;
-let result=num1*num2;
-result=result.toString();
-console.log(result,typeof result)
+    return head;
+};
 
-}
-
-Product("3","5");
+var rotateListWithArray = function(head, array_nodes, last_id) {
+    let penultimate = array_nodes[last_id-1];
+    let last        = array_nodes[last_id];
+    
+    penultimate.next = null;
+    last.next = head;
+    
+    return last;
+};
